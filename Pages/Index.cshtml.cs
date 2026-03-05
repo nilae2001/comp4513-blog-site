9d311b2aa415653ba4540d9a7310bf61cad795eb
nilae2001 <nilaerturk@gmail.com>
2026-03-05 13:54:31 -0800

    project setup

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace comp4513_blog_site.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
